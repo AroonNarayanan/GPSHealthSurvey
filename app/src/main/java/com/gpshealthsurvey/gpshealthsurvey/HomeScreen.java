2ac4a06b5f047053b4bd8ac94e9a963414b86e69
Aroon <aroon@gatech.edu>
2015-03-10 16:39:59 -0400

    SO MUCH ROOM...FOR ACTIVITIES

--- app/src/main/java/com/gpshealthsurvey/gpshealthsurvey/HomeScreen.java
+++ app/src/main/java/com/gpshealthsurvey/gpshealthsurvey/HomeScreen.java
@@ -19,14 +19,20 @@ public class HomeScreen extends ActionBarActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_screen);
 
+        //Add items to home screen menu
         ArrayList<com.gpshealthsurvey.gpshealthsurvey.MenuListItem> menuItems = new ArrayList<com.gpshealthsurvey.gpshealthsurvey.MenuListItem>();
         com.gpshealthsurvey.gpshealthsurvey.MenuListItem markWaypoint = new com.gpshealthsurvey.gpshealthsurvey.MenuListItem("Mark House","mark a household with GPS","mark");
         com.gpshealthsurvey.gpshealthsurvey.MenuListItem sync = new com.gpshealthsurvey.gpshealthsurvey.MenuListItem("Sync","upload and download data","sync");
-        com.gpshealthsurvey.gpshealthsurvey.MenuListItem navigate = new com.gpshealthsurvey.gpshealthsurvey.MenuListItem("Navigate","navigate to a household","nav");
+        com.gpshealthsurvey.gpshealthsurvey.MenuListItem create = new com.gpshealthsurvey.gpshealthsurvey.MenuListItem("Create Survey","create and modify surveys","create");
+        com.gpshealthsurvey.gpshealthsurvey.MenuListItem sample = new com.gpshealthsurvey.gpshealthsurvey.MenuListItem("Sample Data","produce statistical samples from database","sample");
+        //com.gpshealthsurvey.gpshealthsurvey.MenuListItem navigate = new com.gpshealthsurvey.gpshealthsurvey.MenuListItem("Navigate","navigate to a household","nav");
         menuItems.add(markWaypoint);
         menuItems.add(sync);
-        menuItems.add(navigate);
+        menuItems.add(sample);
+        menuItems.add(create);
+        //menuItems.add(navigate);
 
+        //Attach menu items to click listeners
         com.gpshealthsurvey.gpshealthsurvey.MenuAdaptor adaptor = new com.gpshealthsurvey.gpshealthsurvey.MenuAdaptor(menuItems);
         ListView menuView = (ListView) findViewById(R.id.menuList);
         menuView.setAdapter(adaptor);
@@ -34,8 +40,19 @@ public class HomeScreen extends ActionBarActivity {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 if(position==0){
+                    //Mark Household
                     Intent markIntent = new Intent(parent.getContext(),MarkHouse.class);
                     startActivity(markIntent);
+                }
+                if(position==1) {
+                    //Sync Data
+                    Intent syncIntent = new Intent(parent.getContext(),Sync.class);
+                    startActivity(syncIntent);
+                }
+                if(position==2) {
+                    //Sample Data
+                    Intent sampleIntent = new Intent(parent.getContext(),SampleData.class);
+                    startActivity(sampleIntent);
                 }
             }
         });
